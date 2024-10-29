9e7f3242df9aa24f1bbaa4373b02c4056d8e3e90
Mar Acero <mar.acero@enti.cat>
2024-10-29 14:37:40 +0100

    Ejercicio de fors

---
+++ bomba.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "¿Desde qué número quieres que empiece?" | cowsay
+#esto es un comentario
+read CANTIDAD
+
+#for NUMERO in $(seq 10) 
+for NUMERO in `seq 0 $CANTIDAD | sort -hr`
+do
+	clear
+	echo $NUMERO | cowsay
+	sleep 1
+done
+
+clear
+echo -e "\e[33m YELLOW" "BOOOOM!" | cowsay -d
